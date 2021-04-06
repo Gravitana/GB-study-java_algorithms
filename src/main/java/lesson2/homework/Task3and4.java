4ec541f419834ee48566695e6c00cc7581882378
Vitaliy <v.a.grahov@ya.ru>
2021-04-07 01:24:37 +0500

    task3

--- src/main/java/lesson2/homework/Task3and4.java
+++ src/main/java/lesson2/homework/Task3and4.java
@@ -3,7 +3,7 @@ package lesson2.homework;
 import java.util.Arrays;
 import java.util.Random;
 
-public class Task3 {
+public class Task3and4 {
     /*
         Задание 2.3
         Создайте массив размером 400 элементов.
@@ -28,6 +28,14 @@ public class Task3 {
 
         System.out.println(Arrays.toString(mArray));
 
+    /*
+        Задание 2.4
+        На основе существующего массива данных из задания 2.3 реализуйте
+        алгоритм сортировки пузырьком.
+        Оцените сортировку с помощью базового класса System.nanoTime().
+        Сравните время выполнения алгоритмы сортировки методом sort() из
+        задания 2.1 и сортировку пузырьком.
+    */
 
 
     }
